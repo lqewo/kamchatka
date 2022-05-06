f63fdd3fb7af37e77aeceb2f1a312374742df276
lqewo <vrkonosha@gmail.com>
2022-05-06 22:06:04 +0300

    added header & adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,27 @@
 import * as flsFunction from "./modules/functions.js";
-
+import $ from "jquery";
 flsFunction.isWebp();
 
  import Swiper, { Navigation, Pagination } from 'swiper';
 
-  const swiper = new Swiper();
+new Swiper('.swiper', {
+    modules: [Navigation],
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+  });
+
+  $(function() {
+    $('.header__burger-menu').on('click', function (e) {
+      e.preventDefault()
+      $('.header__burger-menu-link, .burger-menu' ).toggleClass('active');
+      $('body').toggleClass('lock');
+
+      })
+  })
+
